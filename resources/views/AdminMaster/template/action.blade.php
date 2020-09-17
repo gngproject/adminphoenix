d80175ece7a2c15e9e574c34f7e7da5d7529bb48
alvin2723 <alvinjulian87@gmail.com>
2020-09-17 09:09:06 +0700

    update tombol activenonactive

--- resources/views/AdminMaster/template/action.blade.php
+++ resources/views/AdminMaster/template/action.blade.php
@@ -1,2 +1,7 @@
 <a href="{{ route('adminmaster.productmaster.view', $model) }}" class="btn btn-sm btn-block btn-success">View</a>
 <a href="{{ route('adminmaster.productmaster.edit', $model) }}"class="btn btn-sm btn-block btn-warning">Edit</a>
+@if($model->status == 1)
+    <a href="{{ route('adminmaster.productmaster.status', $model) }}" class="btn btn-sm btn-block btn-danger">Non-Active</a>
+@else
+    <a href="{{ route('adminmaster.productmaster.status', $model) }}" class="btn btn-sm btn-block btn-primary">Active</a>
+@endif
