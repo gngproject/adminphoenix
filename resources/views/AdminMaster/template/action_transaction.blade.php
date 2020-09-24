cf9f91e8d4eecf83a136a4071e9093f5ff1fa8f1
alvin2723 <alvinjulian87@gmail.com>
2020-09-24 09:43:21 +0700

    update action pengiriman

--- resources/views/AdminMaster/template/action_transaction.blade.php
+++ resources/views/AdminMaster/template/action_transaction.blade.php
@@ -1,4 +1,4 @@
-@if($model)
+@if($model->status != 'success')
     <a href="{{ route('adminmaster.pengirim.resi', $model->TransactionID) }}" class="btn btn-sm btn-block btn-success">Procced to Shipment</a>
 @else
     <a href="{{ route('adminmaster.pengirim.resi', $model) }}" class="btn btn-sm btn-block btn-primary disabled">Shipping</a>
