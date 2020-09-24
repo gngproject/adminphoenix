731f4c1e2932e05dca75260c6d759601711aca94
alvin2723 <alvinjulian87@gmail.com>
2020-09-24 10:03:32 +0700

    update template transaction

--- resources/views/AdminMaster/template/action_transaction.blade.php
+++ resources/views/AdminMaster/template/action_transaction.blade.php
@@ -1,5 +1,7 @@
-@if($model->status != 'success')
-    <a href="{{ route('adminmaster.pengirim.resi', $model->TransactionID) }}" class="btn btn-sm btn-block btn-success">Procced to Shipment</a>
+@if($model->status == 'success')
+<a href="{{ route('adminmaster.pengirim.resi', $model->TransactionID) }}" class="btn btn-sm btn-block btn-primary disabled">Shipping</a>
 @else
-    <a href="{{ route('adminmaster.pengirim.resi', $model) }}" class="btn btn-sm btn-block btn-primary disabled">Shipping</a>
+<a href="{{ route('adminmaster.pengirim.resi', $model->TransactionID) }}" class="btn btn-sm btn-block btn-success">Procced to Shipment</a>
+
+
 @endif
